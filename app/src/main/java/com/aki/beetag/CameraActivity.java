addca5d4b4e1c19b6f50fa2902b3b8e919f149f3
Aki <akisaksala@gmail.com>
2017-08-02 18:36:21 +0200

    Add camera preview TextureView, get CameraDevice

--- app/src/main/java/com/aki/beetag/CameraActivity.java
+++ app/src/main/java/com/aki/beetag/CameraActivity.java
@@ -1,15 +1,101 @@
 package com.aki.beetag;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.os.HandlerThread;
+import android.support.annotation.NonNull;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.TextureView;
 import android.view.View;
+import android.app.Activity;
+import android.hardware.camera2.CameraManager;
 
-public class CameraActivity extends AppCompatActivity {
+public class CameraActivity extends Activity {
+
+    // TextureView that holds the camera's preview image
+    private TextureView cameraPreview;
+    // listener for the camera's preview image
+    private TextureView.SurfaceTextureListener cameraPreviewListener = new TextureView.SurfaceTextureListener() {
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int width, int height) {
+            setupCamera(width, height);
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture surfaceTexture, int width, int height) {
+
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
+            return false;
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {
+
+        }
+    };
+
+    private CameraDevice camera;
+    private String cameraId;
+    // listener for the camera
+    private CameraDevice.StateCallback cameraStateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(@NonNull CameraDevice cameraDevice) {
+            camera = cameraDevice;
+        }
+
+        @Override
+        public void onDisconnected(@NonNull CameraDevice cameraDevice) {
+            cameraDevice.close();
+            camera = null;
+        }
+
+        @Override
+        public void onError(@NonNull CameraDevice cameraDevice, int i) {
+
+        }
+    };
+
+    private HandlerThread backgroundHandlerThread;
+    private Handler backgroundHandler;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+        // assign the camera preview TextureView
+        cameraPreview = (TextureView) findViewById(R.id.textureview_camera_preview);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        startBackgroundThread();
+
+        // check if the camera preview TextureView is available or not
+        if (cameraPreview.isAvailable()) {
+            setupCamera(cameraPreview.getWidth(), cameraPreview.getHeight());
+        }
+        else {
+            // TextureView is not available, set up listener
+            cameraPreview.setSurfaceTextureListener(cameraPreviewListener);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        closeCamera();
+        stopBackgroundThread();
+        super.onPause();
     }
 
     @Override
@@ -25,5 +111,44 @@ public class CameraActivity extends AppCompatActivity {
                     | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                     | View.SYSTEM_UI_FLAG_LAYOUT_STABLE );
         }
+    }
+
+    private void setupCamera(int width, int height) {
+        CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            for (String id : cameraManager.getCameraIdList()) {
+                CameraCharacteristics cameraCharacteristics = cameraManager.getCameraCharacteristics(id);
+                if (cameraCharacteristics.get(CameraCharacteristics.LENS_FACING) == CameraCharacteristics.LENS_FACING_FRONT) {
+                    continue;
+                }
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // release the camera ressource
+    private void closeCamera() {
+        if (camera != null) {
+            camera.close();
+            camera = null;
+        }
+    }
+
+    private void startBackgroundThread() {
+        backgroundHandlerThread = new HandlerThread("BackgroundThread");
+        backgroundHandlerThread.start();
+        backgroundHandler = new Handler(backgroundHandlerThread.getLooper());
+    }
+
+    private void stopBackgroundThread() {
+        backgroundHandlerThread.quitSafely();
+        try {
+            backgroundHandlerThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        backgroundHandlerThread = null;
+        backgroundHandler = null;
     }
 }
