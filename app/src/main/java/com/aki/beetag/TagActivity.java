032bdf299518e2fbf87adb13e8fdd3139af34890
Aki <akisaksala@gmail.com>
2017-10-06 23:58:14 +0200

    Improve zoom behaviour in TagActivity

--- app/src/main/java/com/aki/beetag/TagActivity.java
+++ app/src/main/java/com/aki/beetag/TagActivity.java
@@ -7,11 +7,9 @@ import android.os.Bundle;
 
 import com.davemorrissey.labs.subscaleview.ImageSource;
 import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
-import com.github.chrisbanes.photoview.PhotoView;
 
 public class TagActivity extends Activity {
 
-    //private PhotoView photoView;
     private SubsamplingScaleImageView imageView;
 
     @Override
@@ -22,10 +20,11 @@ public class TagActivity extends Activity {
         Intent intent = getIntent();
         Uri imageUri = intent.getData();
 
-        //photoView = findViewById(R.id.photo_view);
-        //photoView.setImageURI(imageUri);
-
         imageView = findViewById(R.id.imageView);
+        imageView.setPanLimit(SubsamplingScaleImageView.PAN_LIMIT_CENTER);
+        imageView.setDoubleTapZoomStyle(SubsamplingScaleImageView.ZOOM_FOCUS_CENTER);
+        imageView.setOrientation(SubsamplingScaleImageView.ORIENTATION_USE_EXIF);
         imageView.setImage(ImageSource.uri(imageUri));
+
     }
 }
