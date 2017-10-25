00c18f45beca620a2ac16183f62b71cca6f31e27
Aki <akisaksala@gmail.com>
2017-10-25 21:59:36 +0200

    Write bitmap to HTTP output stream

--- app/src/main/java/com/aki/beetag/TagDecodingServerHandler.java
+++ app/src/main/java/com/aki/beetag/TagDecodingServerHandler.java
@@ -1,29 +1,53 @@
 package com.aki.beetag;
 
+import android.graphics.Bitmap;
 import android.net.Uri;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.HashMap;
 
 public class TagDecodingServerHandler {
 
-    private void SendRequestForResult(URL url, Uri imageUri) {
+    private void SendRequestForResult(URL url, Bitmap image) {
         try {
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
+            connection.setRequestMethod("POST");
             connection.setRequestProperty("Content-Type", "application/octet-stream");
+            connection.setDoOutput(true);
+            connection.setChunkedStreamingMode(0);
+            OutputStream out = new BufferedOutputStream(connection.getOutputStream());
+            image.compress(Bitmap.CompressFormat.PNG, 100, out);
+            InputStream in = new BufferedInputStream(connection.getInputStream());
+            // TODO: read input stream, unpack with messagepack
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
-    // from a HashMap of key/value pairs, return the percent encoded URL query string
-    private String getUrlParamsString(HashMap<String, String> params) {
+    private URL buildUrl() throws JSONException, MalformedURLException {
+        String address = "http://tonic.imp.fu-berlin.de:10000/process";
+
+        JSONArray output = new JSONArray(new String[] {"IDs"});
+        HashMap<String, String> params = new HashMap<>();
+        params.put("output", output.toString());
+
+        return new URL(address + buildUrlParamsString(params));
+    }
+
+    // from a HashMap of key/value pairs, return the (percent encoded) URL query string
+    private String buildUrlParamsString(HashMap<String, String> params) {
         StringBuilder stringBuilder = new StringBuilder();
         boolean first = true;
         for (HashMap.Entry<String, String> entry : params.entrySet()) {
