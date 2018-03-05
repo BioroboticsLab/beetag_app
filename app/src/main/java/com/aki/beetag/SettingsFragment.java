657a127320d75a4603bd9eeb0fe8004a71e92fe7
Aki <akisaksala@gmail.com>
2018-03-05 01:22:35 +0100

    Update summary if setting is changed

--- app/src/main/java/com/aki/beetag/SettingsFragment.java
+++ app/src/main/java/com/aki/beetag/SettingsFragment.java
@@ -1,11 +1,14 @@
 package com.aki.beetag;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
 import android.view.View;
 
-public class SettingsFragment extends PreferenceFragment {
+public class SettingsFragment
+        extends PreferenceFragment
+        implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -13,11 +16,34 @@ public class SettingsFragment extends PreferenceFragment {
 
         // load preferences
         addPreferencesFromResource(R.xml.preferences);
+
+        SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
+        findPreference("pref_decoding_server_url")
+                .setSummary(sharedPreferences.getString("pref_decoding_server_url", null));
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         view.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals("pref_decoding_server_url")) {
+            findPreference(key).setSummary(sharedPreferences.getString(key, null));
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+        super.onPause();
     }
 }
